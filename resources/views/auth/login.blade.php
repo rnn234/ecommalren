dbfe4b4e1adefb2db0907d79acdbb2b42c591be8
rnn234 <renaldiaditya857@gmail.com>
2025-01-27 23:47:50 +0700

    ini co

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,27 +17,52 @@
 
 <body>
     <div class="login-content">
+        <!-- Bagian Kiri -->
         <div class="login-content-kiri">
-            <h1> Your <span>Shopping</span>
-                <br>
-                adventure begins here
-            </h1>
+            <h1>Your <span>Shopping</span><br>adventure begins here</h1>
             <p>Login to access</p>
             <img src="{{ asset('assets/img/loginvektor.png') }}" alt="">
         </div>
+
+        <!-- Bagian Kanan -->
         <div class="login-content-kanan">
             <div class="login-form">
                 <h3>Sign In</h3>
-                <form action="">
-                    <input type="text" name="" id="username" placeholder="Username">
-                    <input type="password" name="" id="password" placeholder="Password">
+                <x-validation-errors class="mb-4" /> <!-- Validasi Error -->
+
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <!-- Form Login -->
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <input type="email" id="email" name="email" class="input-field" 
+                        placeholder="Email" value="{{ old('email') }}" required autofocus autocomplete="username">
+                    
+                    <input type="password" id="password" name="password" class="input-field" 
+                        placeholder="Password" required autocomplete="current-password">
+                    
+                    {{-- <label for="remember_me" class="flex items-center">
+                        <input id="remember_me" type="checkbox" class="remember-me-checkbox" name="remember">
+                        <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    </label> --}}
+                    
                     <input type="submit" value="Sign In" class="button">
+
+                    <!-- Forgot Password -->
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}" class="button-forgot-password">Forgot your password?</a>
+                    @endif
                 </form>
+
+                <!-- Sign Up -->
                 <a href="/register" class="button-signup">Sign Up</a>
             </div>
         </div>
     </div>
-
 </body>
 
 </html>
